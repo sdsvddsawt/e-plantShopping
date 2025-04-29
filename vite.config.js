8c2a5b80aeaf0385bc3a85825e36ccb1420362fd
eddiE <62446617+sdsvddsawt@users.noreply.github.com>
2025-04-30 03:49:26 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
-  base: "/REPOSITORY_NAME",
+  base: "/e-plantShopping/",
   plugins: [react()],
 })
